2e80108caa246414c692650f6264a5531040ba15
Nosov Aleksey Valeryevich <a.v.nosov@tinkoff.ru>
2019-05-11 19:47:01 +0300

    fixed RP_ID and ORIGIN, added notification about wrong secrets configuration

--- server/constants/secrets.ts
+++ server/constants/secrets.ts
@@ -1,5 +1,6 @@
 import logger from '../utils/logger';
 import dotenv from 'dotenv';
+import nodeNotifier from 'node-notifier';
 import fs from 'fs';
 
 if (fs.existsSync('.env')) {
@@ -14,15 +15,24 @@ const prod = ENVIRONMENT === 'production';
 
 export const SESSION_SECRET = process.env['SESSION_SECRET'];
 export const MONGODB_URI = prod ? process.env['MONGODB_URI'] : process.env['MONGODB_URI_LOCAL'];
-export const RP_ID = process.env.RP_ID || 'http://localhost:4000';
+export const RP_ID = process.env.RP_ID || 'localhost';
+export const ORIGIN = process.env.ORIGIN || 'http://localhost:3000';
 export const RP_NAME = process.env.RP_NAME || 'webauthn-example';
 
 if (!SESSION_SECRET) {
     logger.error('No client secret. Set SESSION_SECRET environment variable.');
+    nodeNotifier.notify({
+        title: 'WebAuthn Example',
+        message: 'WebAuthn Example can\'t locate SESSION_SECRET',
+    });
     process.exit(1);
 }
 
 if (!MONGODB_URI) {
     logger.error('No mongo connection string. Set MONGODB_URI environment variable.');
+    nodeNotifier.notify({
+        title: 'WebAuthn Example',
+        message: 'WebAuthn Example can\'t locate MONGODB_URI',
+    });
     process.exit(1);
 }
