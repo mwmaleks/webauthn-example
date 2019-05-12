742ab429f9ffa611a05b9cb17e0615a3bd18cc12
Nosov Aleksey Valeryevich <a.v.nosov@tinkoff.ru>
2019-05-12 14:31:00 +0300

    store creds to user entity, refactoring, types

--- server/models/User.ts
+++ server/models/User.ts
@@ -4,7 +4,9 @@ import mongoose from 'mongoose';
 import { Fido2Lib }  from 'fido2-lib';
 import base64url from 'base64url';
 import createRandomBase64URLBuffer from '../utils/createRandomBase64URLBuffer';
-import { DefaultAttestationParams } from '../constants/constants';
+import { DEFAULT_ATTESTATION_OPTIONS, DefaultAttestationOptions } from '../constants/constants';
+import logger from '../utils/logger';
+import { RP_NAME, RP_ID, ORIGIN } from '../constants/secrets';
 
 export type AuthToken = {
     accessToken: string,
@@ -16,7 +18,7 @@ type PubKeyCredParams = {
     alg: number;
 };
 
-type Options = {
+export type Options = {
     rp: {
         name: string;
         id: string;
@@ -26,7 +28,7 @@ type Options = {
         name: string;
         displayName: string;
         email: string;
-        authenticators: string[];
+        authenticators: any;
         challenge: string;
         origin: string;
     };
@@ -40,54 +42,122 @@ type Options = {
         userVerification: string;
     };
 };
-type CreateAttestationParams = {
-    rpId: string;
-    rpName: string;
-    origin: string;
-} & DefaultAttestationParams;
 
-type CreateAttestationOptions = (params: CreateAttestationParams) => Promise<Options>;
+// fixme types
+type AttestationObject = Object;
+
+export type PublicKey = {};
+
+type CreateAttestationOptions = () => Promise<Options>;
+type ValidateAttestationObject = (challenge: string, attestation: AttestationObject) => Promise<UserModel>;
+
+type Cred = {
+    fmt: string;
+    counter: number;
+    credId?: Buffer;
+    credentialPublicKeyJwk: {
+        kty: string;
+        alg: string;
+        crv: string;
+        x: string;
+        y: string;
+    },
+    credentialPublicKeyPem: string;
+};
+
+type MapKey = keyof Cred;
 
 export type UserModel = mongoose.Document & {
-    id: string,
-    email: string,
-    challenges: string[],
-    tokens: AuthToken[],
-    createAttestationOptions: CreateAttestationOptions,
-    createAssertionChallenge: Function,
-    verifyAttestation: Function,
-    verifyAssertion: Function,
+    id: string;
+    email: string;
+    name: string;
+    displayName: string;
+    creds: {
+        [id: string]: Partial<Cred> & { id: string };
+    };
+    lastCredId: string;
+    tokens: AuthToken[];
+    createAttestationOptions: CreateAttestationOptions;
+    validateAttestationObject: ValidateAttestationObject;
+    createAssertionChallenge: Function;
+    verifyAttestation: Function;
+    verifyAssertion: Function;
 };
 
 const userSchema = new mongoose.Schema({
     id: String,
     email: { type: String, unique: true },
+    name: String,
+    displayName: String,
     passwordResetToken: String,
     passwordResetExpires: Date,
-    challenges: Array,
+    creds: Object,
+    lastCredId: String,
     tokens: Array,
 }, { timestamps: true });
 
-userSchema.methods.createAttestationOptions = function (params: CreateAttestationParams) {
-    const f2l = new Fido2Lib(params);
+const f2l = new Fido2Lib({
+    rpId: RP_ID,
+    rpName: RP_NAME,
+    origin: ORIGIN,
+    ...DEFAULT_ATTESTATION_OPTIONS,
+});
+
+userSchema.methods.createAttestationOptions = function () {
     this.id = createRandomBase64URLBuffer();
 
-    return f2l.attestationOptions()
+    return this.save()
+        .then(() => f2l.attestationOptions())
         .then((options: Options) => {
             const challenge = base64url(options.challenge);
-            this.challenges.push(challenge);
+
+            logger.debug('createAttestationOptions: ', options);
 
             return {
+                ...options,
                 challenge,
-                id: this.id,
-                name: this.name,
-                displayName: this.name,
-                email: this.email,
-                // TODO
-                authenticators: ['sd'],
+                user: {
+                    challenge,
+                    id: this.id,
+                    name: this.name,
+                    displayName: this.name,
+                    email: this.email,
+                    authenticators: [] as any,
+                    origin: ORIGIN,
+                },
             };
         });
 };
+
+userSchema.methods.validateAttestationObject =
+    function (challenge: string, attestation: AttestationObject) {
+
+        return f2l.attestationResult(attestation, {
+            challenge,
+            origin: ORIGIN,
+            factor: 'either',
+        }).then(({ authnrData: result }: { authnrData: Map<MapKey, Cred[MapKey]> }) => {
+            logger.debug('authnrData: ', result);
+            const credentialPublicKeyPem = result.get('credentialPublicKeyPem');
+            const credentialPublicKeyJwk = result.get('credentialPublicKeyJwk');
+            const fmt = result.get('fmt');
+            const counter = result.get('counter');
+            const id = base64url(result.get('credId') as Buffer);
+
+            logger.debug('result: ', result);
+            this.creds = this.creds || {};
+            this.creds[id] = {
+                id,
+                fmt,
+                counter,
+                credentialPublicKeyJwk,
+                credentialPublicKeyPem,
+            };
+            this.lastCredId = id;
+
+            return this.save();
+        });
+    };
 
 const User = mongoose.model<UserModel>('User', userSchema);
 export default User;
