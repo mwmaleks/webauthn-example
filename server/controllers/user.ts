03f5f5affe45fb88d0c7a5665d2606f12558a45c
Nosov Aleksey Valeryevich <a.v.nosov@tinkoff.ru>
2019-05-12 14:29:52 +0300

    move challenge to session, do not pass params to f2l each method call, added logout controller

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,7 +1,7 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 
 // models
-import User, { UserModel, PublicKey } from '../models/User';
+import User, { UserModel } from '../models/User';
 
 // utils
 import createName from '../utils/createName';
@@ -9,10 +9,7 @@ import logger from '../utils/logger';
 import { decode } from '../utils/base64';
 
 // constants
-import { DEFAULT_ATTESTATION_OPTIONS, invalidCredsMessage } from '../constants/constants';
-
-// env
-import { RP_NAME, RP_ID, ORIGIN } from '../constants/secrets';
+import { invalidCredsMessage } from '../constants/constants';
 
 /**
  * POST /checkSession
@@ -30,6 +27,7 @@ export const postSession = (req: Request, res: Response) => {
             email: req.user.email,
         },
     });
+    logger.debug('========================================== postSession');
 };
 
 export const postCreateAttestationChallenge = (req: Request, res: Response) => {
@@ -38,11 +36,14 @@ export const postCreateAttestationChallenge = (req: Request, res: Response) => {
     req.getValidationResult()
         .then((result) => {
             if (!result.isEmpty()) {
-                logger.error('ValidationResult is empty');
+                logger.error('ValidationResult is not empty');
+                logger.error('ValidationResult is: ', result.mapped());
                 res.send({
                     code: 'invalid_email',
                     error: 'Invalid email',
+                    ok: false,
                 });
+                logger.debug('========================================== postCreateAttestationChallenge');
                 return;
             }
 
@@ -55,38 +56,40 @@ export const postCreateAttestationChallenge = (req: Request, res: Response) => {
             const user = new User({
                 email,
                 name: createName(email),
-                challenges: [],
             });
 
             logger.debug('user createName:', user);
 
-            return user.createAttestationOptions({
-                rpId: RP_ID,
-                rpName: RP_NAME,
-                origin: ORIGIN,
-                ...DEFAULT_ATTESTATION_OPTIONS,
-            }).then((options) => {
-                return user.save()
-                    .then(() => {
-                        logger.debug('sending attestation options:', options);
-                        res.json(options);
-                    });
-            });
+            return user.createAttestationOptions()
+                .then((options) => {
+                    // save user id for further login flows, and challenge for to continue registration
+                    req.session.challenge = options.challenge;
+                    req.session.userId = options.user.id;
+                    logger.debug('sending attestation options:', options);
+
+                    res.json(options);
+                    logger.debug('========================================== postCreateAttestationChallenge');
+                });
         })
         .catch((error) => {
             logger.error(error);
             res.status(500);
-            res.send('Error while creating challenge');
+            res.send({
+                error: 'Error while creating challenge',
+                ok: false,
+            });
+            logger.debug('========================================== postCreateAttestationChallenge');
         });
 };
 
 export const postFinishAttestation = (req: Request, res: Response) => {
     req.assert('attestation.id', invalidCredsMessage).isString().isLength({ max: 200, min: 50 });
     req.assert('attestation.rawId', invalidCredsMessage).isString().isLength({ max: 200, min: 50 });
-    req.assert('attestation.response.clientDataJSON', invalidCredsMessage).isString().isLength({ max: 400, min: 50 });
-    req.assert('attestation.response.attestationObject', invalidCredsMessage).isString().isLength({ max: 400, min: 50 });
+    req.assert('attestation.response.clientDataJSON', invalidCredsMessage).isString().isLength({ max: 800, min: 50 });
+    req.assert('attestation.response.attestationObject', invalidCredsMessage)
+        .isString()
+        .isLength({ max: 500, min: 50 });
     req.assert('attestation.type', invalidCredsMessage).isIn('public-key');
-    req.assert('email', 'Email is not valid').isEmail();
 
     const {
         id,
@@ -115,66 +118,96 @@ export const postFinishAttestation = (req: Request, res: Response) => {
         id: decode(id),
         rawId: decode(rawId),
     };
+    const {
+        challenge,
+    } = req.session;
+
+    if (!challenge) {
+        res.send({
+            code: 'invalid_challenge',
+            error: 'Invalid saved challenge',
+            ok: false,
+        });
+        logger.debug('========================================== postFinishAttestation');
+        return;
+    }
 
     req.getValidationResult()
         .then((result) => {
             if (!result.isEmpty()) {
-                logger.error('ValidationResult is empty');
+                logger.error('attestation validationResult is not empty');
+                logger.error('attestation validationResult is: ', result.mapped());
 
                 res.send({
                     code: 'invalid_attestation',
                     error: 'Invalid attestation object',
+                    ok: false,
                 });
-                return Promise.reject(new Error('Invalid attestation object'));
+
+                throw new Error('Invalid attestation object');
             }
 
         })
-        .then(() => User.findOne({ email: req.body.email }, (err: Error, user: UserModel) => {
+        .then(() => User.findOne({ id: req.session.userId }, (err: Error, user: UserModel) => {
             if (err) {
                 res.send({
                     code: 'not_found_user',
                     error: 'User not found',
+                    ok: false,
                 });
 
-                return Promise.reject(new Error('User not found'));
+                throw new Error('User not found');
             }
 
             return user;
         }))
-        .then((user: UserModel) => user.validateAttestationObject({
-            attestation,
-            params: {
-                rpId: RP_ID,
-                rpName: RP_NAME,
-                origin: ORIGIN,
-                ...DEFAULT_ATTESTATION_OPTIONS,
-            },
-            origin: ORIGIN,
-        }))
-        .then(user => new Promise((resolve, reject) => {
+        .then((user: UserModel) => user.validateAttestationObject(challenge, attestation))
+        .then(user => new Promise<void>((resolve, reject) => {
+            // invalidate the challenge
+            delete req.session.challenge;
+            // save credId just signed up to session
+            req.session.credId = user.lastCredId;
+
             logger.debug('new user: ', user);
+
             req.logIn(user, ((err: Error) => {
                 if (!err) {
                     logger.debug('successfully logged in new user');
                     res.send({
-                        code: 'createCreds',
+                        code: 'create_creds',
                         ok: true,
                     });
-                    logger.debug('req.session: ', req.session);
                     resolve();
+                    logger.debug('========================================== postFinishAttestation');
+
                     return;
                 }
 
                 reject(new Error('unsuccessful login'));
             }));
         }))
-        .catch((err) => {
+        .catch((err: Error) => {
             logger.error('err: ', err);
             if (!res.headersSent) {
                 res.send({
                     code: 'failed_attestation',
                     error: 'Failed attestation',
+                    ok: false,
                 });
+                logger.debug('========================================== postFinishAttestation');
             }
         });
+};
+
+export const postLogout = (req: Request, res: Response) => {
+    req.logOut();
+    res.send({
+        code: 'success_logout',
+        ok: true,
+    });
+    logger.debug('logout successful');
+    logger.debug('req.session: ', req.session);
+    logger.debug('req.sessionID: ', req.sessionID);
+    logger.debug('req.user: ', req.user);
+    logger.debug('========================================== logout');
 };
