806470b6359b8acb9c61d8414a4b5ac06d63a122
Nosov Aleksey Valeryevich <a.v.nosov@tinkoff.ru>
2019-05-18 15:52:08 +0300

    added lots of favicon images
    added service worker cache first some static

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('webauthn-demo').then(function(cache) {
+            return cache.addAll(
+                [
+                    '/static/js/0.chunk.js',
+                    '/static/js/bundle.chunk.js',
+                    '/static/js/main.chunk.js',
+                    '/index.html'
+                ]
+            );
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('👷', 'activate', event);
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.open('webauthn-demo').then(function(cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function(response) {
+                    if (event.request.method !== 'POST') {
+                        cache.put(event.request, response.clone());
+                    }
+
+                    return response;
+                });
+            });
+        })
+    );
+});
